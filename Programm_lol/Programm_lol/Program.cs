7eb0dbfcc0cb77b4dc348b24a6f2f872ad8af260
salavat <salavatalyukov882@gmail.com>
2022-09-26 15:16:29 +0300

    вот бы работало

--- Programm_lol/Programm_lol/Program.cs
+++ Programm_lol/Programm_lol/Program.cs
@@ -1,7 +1,7 @@
 ﻿double x, y;
 x = double.Parse(Console.ReadLine());
 y= double.Parse(Console.ReadLine());
-if((x*x<=1 && y>=0 && y<=1 && (Math.Abs(x)+Math.Abs(y)<=1 )) || (x*x<=1 && y<=0 && y<=-1*x*x))
+if((x*x<=1 && y>=0 && y<=1 && (Math.Abs(x)+Math.Abs(y)<=1 )) || (x*x<=1 && y<=0 && y>=-1*x*x))
 {
     Console.WriteLine("yes");
 }
