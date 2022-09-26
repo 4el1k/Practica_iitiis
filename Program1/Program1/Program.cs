92399741b63b251ed059e22d122509deb4eed38b
salavat <salavatalyukov882@gmail.com>
2022-09-26 19:18:04 +0300

    задача найти элемент на k индексе в строчке 12345678910....

---
+++ Program1/Program1/Program.cs
@@ -0,0 +1,10 @@
+﻿//0123456789
+//1234567891011121314151617181920212223......
+//найти число которое стоит на k индексе.
+String str = "";
+int k = int.Parse(Console.ReadLine());
+for (int i = 1; i < k; i++)
+{
+    str=str + i.ToString();
+}
+Console.WriteLine($"{str[k]},{k}");
