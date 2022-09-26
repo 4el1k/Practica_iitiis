19df46ac249fd7c43a01bb10762bd1ed508e0703
salavat <salavatalyukov882@gmail.com>
2022-09-26 15:11:49 +0300

    надеюсь работает

--- Programm_lol/Programm_lol/Program.cs
+++ Programm_lol/Programm_lol/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("shiit");
+﻿double x, y;
+x = double.Parse(Console.ReadLine());
+y= double.Parse(Console.ReadLine());
+if((x*x<=1 && y>=0 && y<=1 && (Math.Abs(x)+Math.Abs(y)<=1 )) || (x*x<=1 && y<=0 && y<=-1*x*x))
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
+
+
+
+
+
