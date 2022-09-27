8efcbb4a6df07a1ce83d0a216149ce53bb99543d
salavat <salavatalyukov882@gmail.com>
2022-09-28 01:53:36 +0300

    работает до 188 (двузначные числа)

--- Program1/Program1/Program.cs
+++ Program1/Program1/Program.cs
@@ -1,10 +1,95 @@
 ﻿//0123456789
 //1234567891011121314151617181920212223......
 //найти число которое стоит на k индексе.
-String str = "";
+//работает до 188
 int k = int.Parse(Console.ReadLine());
-for (int i = 1; i < k; i++)
+int n;
+n = k;
+if (k < 100)
 {
-    str=str + i.ToString();
+    while (n >= 10)
+    {
+        n /= 10;
+    }
 }
-Console.WriteLine($"{str[k]},{k}");
+else
+{
+    n = n - (n % 100) * 100;
+    while (n >= 10)
+    {
+        n /= 10;
+    }
+}
+bool left=false, right=false;
+if ((k % 10 != 0) && ((k - 1) % 10 != 0) && ((k + 1) % 10 != 0))
+{
+    if (n % 2 == 0)
+    {
+        right = true;
+    }
+    else
+    {
+        left = true;
+    }
+    if (right)
+    {
+        if (k % 2 == 0)
+        {
+            int b;
+            b = (k % 10) + (((10 - k % 10)) % 10) / 2;
+            Console.WriteLine(b);
+        }
+        else
+        {
+            int b;
+            b = (k % 10) / 2;
+            Console.WriteLine(b);
+        }
+    }
+    else
+    {
+        if (k % 2 == 0)
+        {
+            int b;
+            b = (k % 10) / 2;
+            Console.WriteLine(b);
+        }
+        else
+        {
+            int b;
+            b = (k % 10) / 2 + 1;
+            Console.WriteLine(b);
+        }
+    }
+}
+else
+{
+    if (k % 10 == 0)
+    {
+        if (n % 2 == 0)
+        {
+            Console.WriteLine(5);
+        }
+        else
+        {
+            Console.WriteLine(0);
+        }
+        Console.WriteLine("первый иф");
+    }
+    if ((k - 1) % 10 == 0)
+    {
+        Console.WriteLine(n / 2);
+        
+    }
+    if ((k + 1) % 10 == 0)
+    {
+        Console.WriteLine(n / 2 + 1);
+    }
+    
+}
+string str = "";
+for (int i = 1; i < k+1; i++)
+{
+    str = str + i.ToString();
+}
+Console.WriteLine(str[k]);
--- Programm_lol/Programm_lol/Program.cs
+++ Programm_lol/Programm_lol/Program.cs
@@ -1,16 +1,77 @@
-﻿double x, y;
-x = double.Parse(Console.ReadLine());
-y= double.Parse(Console.ReadLine());
-if((x*x<=1 && y>=0 && y<=1 && (Math.Abs(x)+Math.Abs(y)<=1 )) || (x*x<=1 && y<=0 && y>=-1*x*x))
+﻿//5
+/*
+float a, x,summ=1;
+x=float.Parse(Console.ReadLine());
+a=float.Parse(Console.ReadLine());
+int i = 0;
+a += 1;
+float powerX = 1, fact=1, peremen_znach;
+while (Math.Pow(1+x,a)!=summ)
 {
-    Console.WriteLine("yes");
+    i++;
+    fact *= i;
+    powerX *= x*(a-i);
+    summ += powerX / fact;
 }
-else
+Console.WriteLine($"{summ},{Math.Pow(1+x,a)}");
+*/
+int x = int.Parse(Console.ReadLine());
+bool flag,flag1;
+for (int i = 2; i < Math.Pow(x,0.5)+1; i++)
 {
-    Console.WriteLine("no");
+    flag1 = true;
+    flag = true;
+    if (x % i == 0)
+    {
+        if (x / i != i)
+        {
+            for (int j = 2; j < Math.Pow(i, 0.5) + 1; j++)
+            {
+                if (j != i)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+                for (int j = 2; j < Math.Pow(x/i, 0.5) + 1; j++)
+            {
+                
+                
+                if ((x / i) % j == 0)
+                {
+                    flag1 = false;
+                    break;
+                }
+                
+            }
+            if (flag)
+            {
+                Console.WriteLine(i);
+            }
+            if (flag1)
+            {
+                Console.WriteLine(x/i);
+            }
+
+
+        }
+        else
+        {
+            for (int j = 2; j < Math.Pow(i, 0.5) + 1; j++)
+            {
+                if (i % j == 0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
 }
-
-
-
-
-
