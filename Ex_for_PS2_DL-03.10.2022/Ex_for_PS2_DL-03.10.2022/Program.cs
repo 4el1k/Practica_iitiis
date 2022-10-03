cdd1656e8108472b7747fcaa8c56b34fc565d60e
salavat <salavatalyukov882@gmail.com>
2022-10-03 21:15:39 +0300

    задача номер 5, дедлайн 03.10.2022

---
+++ Ex_for_PS2_DL-03.10.2022/Ex_for_PS2_DL-03.10.2022/Program.cs
@@ -0,0 +1,20 @@
+﻿//ex 5
+//(1+x)^a; -1<=x<=1
+//Math.Round(a,b). a - число, b - кол-во знаков после запятой.
+float x = 0.25f;
+float a = 4.0f;
+float summ = 1;
+float powerX = 1;
+float factorial = 1;
+float multA = 1;
+int i = 0;
+int k = 3; //кол-во знаков после запятой, нужно для округления.
+while (Math.Round(Math.Pow(1 + x, a),k)!=Math.Round(summ,k))
+{
+    multA *= a - i;
+    powerX *= x;
+    i++;
+    factorial *= i;
+    summ += multA * powerX / factorial;
+}
+Console.WriteLine($"{summ},{Math.Pow(1+x,a)}");
--- Program1/Program1/Program.cs
+++ Program1/Program1/Program.cs
@@ -2,94 +2,20 @@
 //0123456789 1 0  1  1121314151617181920212223......
 //найти число которое стоит на k индексе.
 //работает до 188
-int k = int.Parse(Console.ReadLine());
-int n;
-n = k;
-if (k < 100)
+int k = 1889;
+bool flag = true;
+int n1 = 9;
+int n2 = 0;
+int i = 0;
+while (flag)
 {
-    while (n >= 10)
+    i++;
+    if (k <= n1+n2)
     {
-        n /= 10;
+        flag = false;
+        break;
     }
+    n2 = n1;
+    n1 = n1 * (int)Math.Pow(10, i);
 }
-else
-{
-    n = n - (n % 100) * 100;
-    while (n >= 10)
-    {
-        n /= 10;
-    }
-}
-bool left=false, right=false;
-if ((k % 10 != 0) && ((k - 1) % 10 != 0) && ((k + 1) % 10 != 0))
-{
-    if (n % 2 == 0)
-    {
-        right = true;
-    }
-    else
-    {
-        left = true;
-    }
-    if (right)
-    {
-        if (k % 2 == 0)
-        {
-            int b;
-            b = (k % 10) + (((10 - k % 10)) % 10) / 2;
-            Console.WriteLine(b);
-        }
-        else
-        {
-            int b;
-            b = (k % 10) / 2;
-            Console.WriteLine(b);
-        }
-    }
-    else
-    {
-        if (k % 2 == 0)
-        {
-            int b;
-            b = (k % 10) / 2;
-            Console.WriteLine(b);
-        }
-        else
-        {
-            int b;
-            b = (k % 10) / 2 + 1;
-            Console.WriteLine(b);
-        }
-    }
-}
-else
-{
-    if (k % 10 == 0)
-    {
-        if (n % 2 == 0)
-        {
-            Console.WriteLine(5);
-        }
-        else
-        {
-            Console.WriteLine(0);
-        }
-        Console.WriteLine("первый иф");
-    }
-    if ((k - 1) % 10 == 0)
-    {
-        Console.WriteLine(n / 2);
-        
-    }
-    if ((k + 1) % 10 == 0)
-    {
-        Console.WriteLine(n / 2 + 1);
-    }
-    
-}
-string str = "";
-for (int i = 1; i < k+1; i++)
-{
-    str = str + i.ToString();
-}
-Console.WriteLine(str[k]);
+Console.WriteLine(i);
